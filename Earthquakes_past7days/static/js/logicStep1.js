69ac6a0c94001361f2518f39cfb64a8fb34c7a29
morty101 <belmoudden.hicham@gmail.com>
2023-01-11 18:45:21 +0100

    commit 7

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -19,21 +19,18 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
-    "Satellite Streets": satelliteStreets
+    "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
-
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/hbmbs/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
@@ -43,15 +40,16 @@ let myStyle = {
 };
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data,{
+  L.geoJSON(data).addTo(map);
+});
+
+/* ,{
     style: myStyle,
     onEachFeature: function(feature, layer){
         console.log(feature);
         layer.bindPopup("<h2>"+'Neighborhood: '+feature.properties.AREA_NAME+"</h2>")
     } 
-  }).addTo(map);
-});
-
+  } */
